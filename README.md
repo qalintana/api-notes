dde28bff2b343207f9cbea819798a32211cfdbaf
qalintana <alintana18@gmail.com>
2020-09-29 00:27:52 +0100

    adicionado o readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# API de Anotações
+
+---
+## Model
+> - User
+> - Notes
+
+## Execução
+
+`npm start` de acordo com a configuração encontrada no package.json
+
+
+> **OBS** criar o ficheiro `.env` com a chave *JWT_TOKEN* para a autenticação
+
+- [x] Bora Codar
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
+import cors from 'cors';
 
 // * config dotenv
 import dotenv from 'dotenv';
@@ -15,6 +16,7 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
